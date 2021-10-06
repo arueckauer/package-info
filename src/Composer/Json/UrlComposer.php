f818f61df2fc46810ae92ff4280bd71f1fe02ebf
Andi Rückauer <arueckauer@gmail.com>
2021-10-06 15:18:46 +0200

    Add UrlComposer

---
+++ src/Composer/Json/UrlComposer.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PackageInfo\Composer\Json;
+
+use function sprintf;
+
+class UrlComposer
+{
+    public function __invoke(string $owner, string $repository, string $head): string
+    {
+        return sprintf(
+            'https://raw.githubusercontent.com/%s/%s/%s/composer.json',
+            $owner,
+            $repository,
+            $head
+        );
+    }
+}
---
+++ test/Composer/Json/UrlComposerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PackageInfoTest\Composer\Json;
+
+use PackageInfo\Composer\Json\UrlComposer;
+use PHPUnit\Framework\TestCase;
+
+class UrlComposerTest extends TestCase
+{
+    public static function urls(): array
+    {
+        return [
+            [
+                'https://raw.githubusercontent.com/mezzio/mezzio-twigrenderer/2.8.x/composer.json',
+                'mezzio',
+                'mezzio-twigrenderer',
+                '2.8.x',
+            ],
+            [
+                'https://raw.githubusercontent.com/mezzio/mezzio-twigrenderer/2.7.0/composer.json',
+                'mezzio',
+                'mezzio-twigrenderer',
+                '2.7.0',
+            ],
+            [
+                'https://raw.githubusercontent.com/arueckauer/mezzio-twigrenderer/doc/twig-link/composer.json',
+                'arueckauer',
+                'mezzio-twigrenderer',
+                'doc/twig-link',
+            ],
+        ];
+    }
+
+    /**
+     * @covers \PackageInfo\Composer\Json\UrlComposer::__invoke
+     * @dataProvider urls
+     */
+    public function test___invoke(
+        string $expected,
+        string $owner,
+        string $repository,
+        string $head
+    ): void {
+        $urlComposer = new UrlComposer();
+
+        self::assertSame(
+            $expected,
+            $urlComposer($owner, $repository, $head)
+        );
+    }
+}
