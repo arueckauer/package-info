e63cc92c259408fa4dbd438b2b87d1700aad512d
Andi Rückauer <arueckauer@gmail.com>
2024-10-14 08:30:29 +0200

    Adds bin and config paths to Rector configuration Changes PHP version to 8.2 in Rector configuration Adds rules for PHPUnit 10.0 to Rector configuration

--- rector.php
+++ rector.php
@@ -3,10 +3,13 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
+        __DIR__ . '/bin',
+        __DIR__ . '/config',
         __DIR__ . '/src',
         __DIR__ . '/test',
     ]);
@@ -14,6 +17,7 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->disableParallel();
 
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_74,
+        LevelSetList::UP_TO_PHP_82,
+        PHPUnitSetList::PHPUNIT_100,
     ]);
 };
